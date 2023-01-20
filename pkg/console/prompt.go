b77d99db644a0e0bfe7fa636f14ac1b27e7f5f3b
pspiagicw <pspiagicw@gmail.com>
2023-01-20 10:26:32 +0530

    Changed ASCII art

--- pkg/console/prompt.go
+++ pkg/console/prompt.go
@@ -3,17 +3,23 @@ package console
 import "fmt"
 
 const BANNER string = `
-░▄▀▄░█▀▀░█▄█░█▀█░█▀█░▀█▀░█▀▄░█▀█
-░█\█░█▀▀░█░█░█▀█░█░█░░█░░█▀▄░█▀█
-░░▀\░▀▀▀░▀░▀░▀░▀░▀░▀░░▀░░▀░▀░▀░▀`
+                                 | |            
+  __ _  ___ _ __ ___   __ _ _ __ | |_ _ __ __ _ 
+ / _' |/ _ \ '_ ' _ \ / _' | '_ \| __| '__/ _' |
+| (_| |  __/ | | | | | (_| | | | | |_| | | (_| |
+ \__, |\___|_| |_| |_|\__,_|_| |_|\__|_|  \__,_|
+    | |                                         
+    |_|
+`
 
 func ShowBanner(version string) {
-	fmt.Println(BANNER)
+	fmt.Print(BANNER)
 	fmt.Println()
-	fmt.Printf("Version %s\n", version)
-	fmt.Println("Control QEMU like magic!")
-	fmt.Println("Welcome to qemantra")
-	fmt.Println("Run `qemantra -h` for help")
+	fmt.Printf("\tVersion %s\n", version)
+	fmt.Println("\tControl QEMU like magic!")
+	fmt.Println("\tWelcome to qemantra")
+	fmt.Println("\tRun `qemantra -h` for help")
+	fmt.Println()
 }
 func ShowSubcommands() {
 	fmt.Println("Available subcommands are [list,run,create,rename,edit]")
